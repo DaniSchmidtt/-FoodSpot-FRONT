3688d54cb4d7e9e80b973af927f366f444ce2015
Danilo Schmidt Bueno <daniloschmidtbueno@gmail.com>
2021-11-21 12:24:01 -0300

    Versionamento do Front com a camada de desenvolvimento

--- src/app/restaurantes/restaurantes.page.ts
+++ src/app/restaurantes/restaurantes.page.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵɵsetComponentScope } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.page.html',
   styleUrls: ['./restaurantes.page.scss'],
 })
+
 export class RestaurantesPage implements OnInit {
   public restaurante: string;
-  numbers: any[];
+  public api: Observable<any>;
+  public retorno;
 
-  constructor(private activatedRoute: ActivatedRoute) {
-      this.numbers = Array(5).fill(4); // Isso é só pra aparecer o design, trocar quando implementar a listagem de verdade :)
-    
+  constructor(private activatedRoute: ActivatedRoute,public httpClient: HttpClient) {
+      this.httpClient.get("http://localhost:1337/restaurante")
+      .subscribe(data => {
+        this.retorno = data;
+        console.log(data)
+      }, error => {
+        console.log(error);
+      });
    }
 
   ngOnInit() {
